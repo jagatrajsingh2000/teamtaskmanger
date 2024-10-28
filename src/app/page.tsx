c13bf8cfe6d674edb9d44048165211ed9ee8dc2f
jagatrajsingh2000 <jagatrajsingh2000@gmail.com>
2024-10-28 16:12:30 +0530

    button components created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 export default function Home() {
-  return (
+  return (<div className="flex">
+    <Button variant={'primary'}>click me</Button>
     <Button variant={'destructive'}>click me</Button>
+    <Button variant={'outline'}>click me</Button>
+    <Button variant={'secondary'}>click me</Button>
+    <Button variant={'ghost'}>click me</Button>
+    <Button variant={'muted'}>click me</Button>
+    <Button variant={'ghost'}>click me</Button>
+    <Button variant={'teritarry'}>click me</Button>
+    </div>
   );
 }
